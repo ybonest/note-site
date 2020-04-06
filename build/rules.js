5eb9928a35ec51a5e75425603183ba0372c1b8ba
ybonest <ybogitchi@yeah.net>
2020-04-06 20:39:19 +0800

    fix image url

--- build/rules.js
+++ build/rules.js
@@ -32,8 +32,10 @@ module.exports.img = {
   test: /\.(png|jpg|gif)$/,
   use: [
     {
-      loader: 'file-loader',
-      options: {},
+      loader: 'url-loader',
+      options: {
+        limit: 8192
+      }
     },
   ]
 }
